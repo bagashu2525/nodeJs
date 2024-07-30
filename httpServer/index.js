be0bca6f28b9fea02ad63f422bc5053ecfad13ff
bagashu2525 <bagasmita25@gmal.com>
2024-07-30 16:57:06 +0530

    URL routing

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,9 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((request,response)=>{
-    response.end("Hello world");
+    if(request.url=='/'){
+        response.end(`<h1> the url is home route</h1>`)
+    }
+    else if(request.url=='/product'){
+            response.end(`<h1> this is a product second version</h1>`)
+    }
+    else{
+        response.writeHead(404,{"Content-type":"text/html"});
+         response.end("<h1>404 not found</h1>")
+    }
+    // response.end("Hello world");
 });
 
 server.listen(8000,"127.0.0.1",()=>{
-    console.log("listening to port no. 8000")
+    console.log("listening to port no. 8000 : http://localhost:8000")
 });
